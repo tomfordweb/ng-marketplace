689b34a8d6804c9123cd1420da8b4867a4e57483
Tom Ford <tomforweb@gmail.com>
2021-03-07 12:25:20 -0500

    this is a lot simpler if i don't try and be cute by hiding the pokemon id

--- src/app/components/pokemon-detail/pokemon-detail.component.ts
+++ src/app/components/pokemon-detail/pokemon-detail.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { select, Store } from "@ngrx/store";
 import { of, throwError } from "rxjs";
-import { switchMap, tap } from "rxjs/operators";
+import { filter, switchMap, tap } from "rxjs/operators";
 import { PokemonService } from "src/app/lib/pokemon/pokemon.service";
+import { selectActivePokemonByRouterParam } from "src/app/state/pokemon.selector";
 
 @Component({
   selector: "app-pokemon-detail",
@@ -10,17 +11,14 @@ import { PokemonService } from "src/app/lib/pokemon/pokemon.service";
   styleUrls: ["./pokemon-detail.component.scss"],
 })
 export class PokemonDetailComponent implements OnInit {
-  // pokemon$ = this.store.pipe(
-  //   select(selectActivePokemonByRouterParam),
-  //   tap((pokedex) => {
-  //     if (!pokedex) {
-  //       throwError("missing pokedex");
-  //     }
-  //   })
-  //   // switchMap((pokemon) => {
-  //   //   return this.pokemonService.getPokemonById(pokemon.id);
-  //   // })
-  // );
+  pokemon$ = this.store.pipe(
+    select(selectActivePokemonByRouterParam),
+    filter((pokemon) => pokemon !== undefined),
+    switchMap((pokemon) => {
+      console.log(pokemon);
+      return this.pokemonService.getPokemonById(pokemon.id);
+    })
+  );
   constructor(private pokemonService: PokemonService, private store: Store) {}
 
   ngOnInit(): void {}
